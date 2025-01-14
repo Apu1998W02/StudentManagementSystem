18b58ca2fe95df62a0fd5dfb72e5cc01ffd41fa3
Apeksha <apekshawarnakulasooriya98@gmail.com>
2025-01-14 13:15:46 +0530

    First Commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'student-details-test',
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.scss'
+})
+export class TestComponent {
+
+}
